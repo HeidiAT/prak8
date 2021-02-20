13345c2796e57406a89d51f4e41a89e7c0cb739f
HeidiAT <angeltengriano@gmail.com>
2021-02-20 19:31:03 +0800

    first commit

---
+++ application/controllers/api/Members.php
@@ -0,0 +1,18 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+
+class Members extends REST_Controller
+{
+
+  function __construct()
+  {
+    parent::__construct();
+    $this->load->model('member_model');
+  }
+
+  public function memberslist_get()
+  {
+    $data = $this->member_model->memberslist();
+    $this->response( ['members'=> $data], 200);
+  }
+}
